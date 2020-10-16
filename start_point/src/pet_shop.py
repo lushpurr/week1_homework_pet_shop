1be8fda3ab8249461f482d394faaa506dd7cae9a
lushpurr <lushpurr@gmail.com>
2020-10-16 18:25:31 +0100

    functions 1, 2, 3, 5 passed test

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -2,6 +2,15 @@
 def get_pet_shop_name(list):
     return list["name"]
 
-
 def get_total_cash(list):
     return list["admin"]["total_cash"]
+
+def add_or_remove_cash(list, amount):
+    list["admin"]["total_cash"] = get_total_cash(list) + amount
+
+
+def get_pets_sold(list):
+    return list["admin"]["pets_sold"]
+
+# def increase_pets_sold(list, pets_increase): #append pets_sold by 2
+#     return list["admin"]["pets_sold"].append(pets_increase)
