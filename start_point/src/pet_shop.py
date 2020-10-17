cf1a17197342c34ed46df27354b3928b1b3b4c80
lushpurr <lushpurr@gmail.com>
2020-10-17 17:19:40 +0100

    1-9 completed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -11,5 +11,31 @@ def add_or_remove_cash(shop, amount):
 def get_pets_sold(shop):
     return shop["admin"]["pets_sold"]
 
-def increase_pets_sold(shop, add_pet_sold):
-    shop["admin"]["pets_sold"] = get_pets_sold(shop) + add_pet_sold
+def increase_pets_sold(shop, pet_sold_increase):
+    shop["admin"]["pets_sold"] = get_pets_sold(shop) + pet_sold_increase
+
+def get_stock_count(shop):
+    return len(shop["pets"])
+
+def get_pets_by_breed(shop, breed):
+    breed_count = []
+    for pet in shop["pets"]:
+        if pet["breed"] == breed:
+            breed_count.append(pet)
+    return breed_count
+
+# def find_pet_by_name(shop, pet_name):
+
+
+
+
+
+# Simpler version:
+# def all_favourite_foods(people):
+#   favourite_foods = []
+#   for person in people:
+#     favourite_foods.extend(person["favourites"]["snacks"])
+  
+#   return favourite_foods
+
+
