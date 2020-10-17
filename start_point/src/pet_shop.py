a626753e393193e3cb24b5175524cb32d483395b
lushpurr <lushpurr@gmail.com>
2020-10-17 15:45:43 +0100

    1-6 passed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,12 +1,15 @@
 # WRITE YOUR FUNCTIONS HERE
-def get_pet_shop_name(list):
-    return list["name"]
+def get_pet_shop_name(shop):
+    return shop["name"]
 
-def get_total_cash(list):
-    return list["admin"]["total_cash"]
+def get_total_cash(shop):
+    return shop["admin"]["total_cash"]
 
-def add_or_remove_cash(list, amount):
-    list["admin"]["total_cash"] = get_total_cash(list) + amount
+def add_or_remove_cash(shop, amount):
+    shop["admin"]["total_cash"] = get_total_cash(shop) + amount
 
-def get_pets_sold(list):
-    return list["admin"]["pets_sold"]
+def get_pets_sold(shop):
+    return shop["admin"]["pets_sold"]
+
+def increase_pets_sold(shop, add_pet_sold):
+    shop["admin"]["pets_sold"] = get_pets_sold(shop) + add_pet_sold
