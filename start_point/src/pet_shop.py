9469c24bc6202ca66cc33a156ce4b4de53eb5766
lushpurr <lushpurr@gmail.com>
2020-10-18 13:04:45 +0100

    1 - 19 completed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,3 +1,5 @@
+import pdb
+
 # WRITE YOUR FUNCTIONS HERE
 def get_pet_shop_name(shop):
     return shop["name"]
@@ -38,21 +40,43 @@ def remove_pet_by_name(shop,pet_name):
 def add_pet_to_stock(shop, new_pet):
     shop["pets"].append(new_pet)
 
-def get_customer_cash(shop):
-    return shop["cash"]
+def get_customer_cash(customer):
+    return customer["cash"]
 
-def remove_customer_cash(shop, amount):
-    shop["cash"] -= amount
-    return shop["cash"]
+def remove_customer_cash(customer, amount):
+    customer["cash"] -= amount
+    return customer["cash"]
 
-def get_customer_pet_count(shop):
-    return len(shop["pets"])
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
 
-def add_pet_to_customer(shop, new_pet):
-    shop["pets"].append(new_pet)
-    return len(shop["pets"])
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)
+    return len(customer["pets"])
 
 
+# check customers "cash" and compare it with price of pet
+# if cash is >= pet "price"
+# return true
+
+def customer_can_afford_pet(customers, new_pet):
+    for customer in customers:
+        if customers["cash"] >= new_pet["price"]:
+            return True
+        else:
+            return False
+    
+
+    
+    # pdb.set_trace()
+
+
+
+    # for pet in customer["cash"]:
+    #     if customer["cash"] >= pet["price"]:
+    #         return True
+    #     else: 
+    #         return False
 
 # Simpler version:
 # def all_favourite_foods(people):
